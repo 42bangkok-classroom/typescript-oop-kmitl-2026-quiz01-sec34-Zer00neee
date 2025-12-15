e97ee1bfdc342cb5da4c4e80ad133f47a5f6a218
68010126 <68010126@kmitl.ac.th>
2025-12-15 17:33:53 +0700

    add

---
+++ src/q13/light.ts
@@ -0,0 +1,16 @@
+const input = process.argv[2];
+const color = input ? input.toLowerCase() : "";
+
+switch (color) {
+  case "red":
+    console.log("Stop");
+    break;
+  case "yellow":
+    console.log("Caution");
+    break;
+  case "green":
+    console.log("Go");
+    break;
+  default:
+    console.log("Unknown");
+}
